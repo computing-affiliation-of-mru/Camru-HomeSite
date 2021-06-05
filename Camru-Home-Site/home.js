9cd7db8f6e3d5aa12d32329e651cdaa4f5e3c99b
A <ali520@mtroyal.ca>
2021-06-05 15:03:11 -0600

    Add files via upload

---
+++ Camru-Home-Site/home.js
@@ -0,0 +1,6 @@
+ window.odometerOptions = {
+          selector: '.odometer',
+          format: '(,ddd)', // Change how digit groups are formatted, and how many digits are shown after the decimal point
+          duration: 13000, // Change how long the javascript expects the CSS animation to take
+          theme: 'default'
+        };
