82a5d016e8489fc7de4acd3f029b3c57ad6b8933
Taylor Amy <taylorsamy@users.noreply.github.com>
2021-08-09 20:37:36 -0600

    Delete home.js

--- home.js
+++
@@ -1,6 +0,0 @@
- window.odometerOptions = {
-          selector: '.odometer',
-          format: '(,ddd)', // Change how digit groups are formatted, and how many digits are shown after the decimal point
-          duration: 13000, // Change how long the javascript expects the CSS animation to take
-          theme: 'default'
-        };
